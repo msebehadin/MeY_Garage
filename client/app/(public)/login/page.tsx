0feec905b5176158da8d0d4fab521cfa259ddece
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2025-12-28 10:00:45 +0300

    feat: the login page

--- client/app/(public)/login/page.tsx
+++ client/app/(public)/login/page.tsx
@@ -1,40 +1,15 @@
-"use client";
+import { getSession } from '@/lib/get-session'
+import { redirect } from 'next/navigation';
 
-import { useState } from "react";
-import { useAuthStore } from "@/store/auth.store";
 
-export default function LoginPage() {
-  const login = useAuthStore((s) => s.login);
-  const loading = useAuthStore((s) => s.loading);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    await login(email, password);
-  };
-
+const   LoginPage  = async () => {
+  const session=await getSession();
+  if(session){
+    redirect('/dashboard')
+  }
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button disabled={loading}>
-        {loading ? "Signing in..." : "Login"}
-      </button>
-    </form>
-  );
+    <div>LoginPage</div>
+  )
 }
+
+export default LoginPage
