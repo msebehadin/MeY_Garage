7f35f7b0b273d5221fdff78f1a6b65165102ab95
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2026-01-06 00:15:49 +0300

    feat:order service to the real business logic

---
+++ server/src/services/order.service.ts
@@ -0,0 +1,52 @@
+import { prisma } from "../config/db";
+import { Order, OrderStatus } from "@prisma/client";
+interface createOrderInput{
+    customerId: any;
+    customId:number;
+    vehicleId:number;
+}
+interface AssignOrderInput{
+    orderId:number;
+    mechanicId:string;
+}
+export const createOrder = async (data: createOrderInput):Promise<Order> => {
+    const vehicle=await prisma.vehicle.findFirst({
+        where:{
+            id:data.vehicleId,
+            customerId:data.customId
+        }
+    })
+  if (!vehicle) {
+    throw new Error("Vehicle not found or does not belong to this customer");
+  }
+  return prisma.order.create({
+    data: {
+      orderHash: crypto.randomUUID(),
+      customerId: data.customerId,
+      vehicleId: data.vehicleId,
+      status: OrderStatus.PENDING,
+    },
+  });
+};
+
+export const assignOrder = async ({ orderId, mechanicId }:AssignOrderInput):Promise<Order> => {
+  const mechanic = await prisma.user.findUnique({
+    where: { id: mechanicId },
+  });
+  if (!mechanic || mechanic.role !== "MECHANIC") {
+    throw new Error("invalid mechanic");
+  }
+  return prisma.order.update({
+    where: { id: orderId },
+    data: {
+      assignedToId: mechanicId,
+      status: OrderStatus.IN_PROGRESS,
+    },
+  });
+};
+export const getOrdersForMechanic = async (mechanicId:string):Promise<Order[]> => {
+  return prisma.order.findMany({
+    where: { assignedToId: mechanicId },
+    include: { vehicle: true, customer: true },
+  });
+};
