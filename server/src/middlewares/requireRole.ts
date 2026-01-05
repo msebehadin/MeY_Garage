502116b45bcfed6c41b8be6a63e3b224b314986d
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2026-01-05 21:35:12 +0300

    feat:delete the previous file and update the requireRole

--- server/src/middlewares/requireRole.ts
+++ server/src/middlewares/requireRole.ts
@@ -1,17 +1,14 @@
-import {Request,Response,NextFunction} from 'express'
-import {Role} from '@prisma/client'
-import { auth } from '../config/auth'
-export const requiredRole=(...allowedRoles:string[])=>async (req:Request,res:Response,next:NextFunction)=>{
-    try{
-        const session=await auth.api.getSession({req});
-         if (!session || !session.user) {
-        return res.status(401).json({
-          message: "Unauthorized: Please log in",
-        });
-      }
-      const userRole=session.user.role;
-         if (!userRole || !allowedRoles.includes(userRole)) {
-        return res.status(403).json({
-          message: "Forbidden: Insufficient permissions",
-        });
-      }
+export const requiredRole=(...roles:any)=>{
+  return (req:any,res:any,next:any)=>{
+    const user=req.user;
+    if(!user){
+      return res.status(401).json({message:'unauthorized'})
+    }
+    if(!roles.includes(user.role)){
+      return res.status(403).json({
+        message:'forbidden:insufficient role'
+      })
+    }
+    next()
+  }
+}
